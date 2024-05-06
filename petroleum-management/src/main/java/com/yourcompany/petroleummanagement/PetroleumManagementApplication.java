f9d19393b2a3e9f2db2a1b31fc7f15889e881f93
MALALA-ZO Raharison Fabien <hei.fabien.2@gmail.com>
2024-05-06 15:51:44 +0300

    spring initialized

---
+++ petroleum-management/src/main/java/com/yourcompany/petroleummanagement/PetroleumManagementApplication.java
@@ -0,0 +1,13 @@
+package com.yourcompany.petroleummanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetroleumManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetroleumManagementApplication.class, args);
+	}
+
+}
---
+++ petroleum-management/src/test/java/com/yourcompany/petroleummanagement/PetroleumManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcompany.petroleummanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetroleumManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
